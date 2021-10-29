fc0a678f1118908f2f77f6eed9c9e11fbf090bc7
KAZUMA KURAMOTO <kazumakuramoto@KAZUMAnoMacBook-puro.local>
2021-10-28 22:17:35 -0700

    use displayName

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "highlight.js/styles/dark.css";
 import "../styles/index.scss";
-export default ({ Component, pageProps }) => <Component {...pageProps} />;
+
+const MyComponent = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+MyComponent.displayName = "MyComponent";
+
+export default MyComponent;
