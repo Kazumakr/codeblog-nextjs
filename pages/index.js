10068661683b03d1db82338264938edf6cc36609
KAZUMA KURAMOTO <kazumakuramoto@KAZUMAnoMacBook-puro.local>
2021-10-28 23:36:38 -0700

    changed text

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Home({ blogs }) {
 					<Media className="admin-intro">
 						<Media body>
 							<p className="welcome-text">
-								My name is Kazuma and I am a CICCC student. I am learning web
-								development. This is my blog page.
+								My name is Kazuma and I am a CICCC student. I am learning about
+								web development. Thank you!
 							</p>
 						</Media>
 					</Media>
